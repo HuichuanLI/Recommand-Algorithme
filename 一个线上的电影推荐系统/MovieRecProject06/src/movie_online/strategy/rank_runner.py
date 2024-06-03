21628772ce63cdf51ce97321c432dd3c84bd860b
lhc456 <lhc14124908@163.com>
2024-06-04 00:38:10 +0800

    add

--- 一个线上的电影推荐系统/MovieRecProject06/src/movie_online/strategy/rank_runner.py
+++ 一个线上的电影推荐系统/MovieRecProject06/src/movie_online/strategy/rank_runner.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from typing import List, Dict
 
+from .rank.bpr_rank import BPRRank
 from .rank.dummy_rank import DummyRank
+from .rank.fm_rank import FMRank
+from .rank.gbdt_lr_rank import GBDTLrRank
+from .rank.lr_rank import LrRank
 from .rank.recall_weighting_rank import RecallWeightingRank
 from .. import logger
 from ..entity import InnerRecItem
@@ -14,6 +18,10 @@ class RankRunner(object):
     def __init__(self):
         self.dummy = DummyRank()
         self.recall_weighting = RecallWeightingRank()
+        self.fm = FMRank()
+        self.lr = LrRank()
+        self.gbdt_lr = GBDTLrRank()
+        self.bpr = BPRRank()
 
     def rank_candidates_items(self,
                               scene: SceneMeta, config: ConfigParams,
@@ -30,7 +38,8 @@ class RankRunner(object):
 
     def _rank_items(self,
                     rank, config: ConfigParams,
-                    items: List[InnerRecItem], spus: Dict[int, SpuFeatureEntity]
+                    items: List[InnerRecItem],
+                    spus: Dict[int, SpuFeatureEntity]
                     ) -> List[InnerRecItem]:
         """
         基于具体的排序策略对候选商品列表进行排序操作
@@ -45,6 +54,14 @@ class RankRunner(object):
                 items = self.dummy.rank_items(items)
             elif rank == 'recall_weighting':
                 items = self.recall_weighting.rank_items(items)
+            elif rank == 'fm':
+                items = self.fm.rank_items(items, user=config.user, spus=spus)
+            elif rank == 'lr':
+                items = self.lr.rank_items(items, user=config.user, spus=spus)
+            elif rank == 'gbdt_lr':
+                items = self.gbdt_lr.rank_items(items, user=config.user, spus=spus)
+            elif rank == 'bpr':
+                items = self.bpr.rank_items(items, user=config.user, spus=spus)
             else:
                 logger.error(f"当前系统不支持该排序策略:{rank}")
         except Exception as e:
